261972cb112ca42eb81ddb4f99c08d2f23c630cc
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2024-02-26 00:19:57 -0300

    fix/feat: Renomeando variaveis e nova função.

--- src/likes.js
+++ src/likes.js
@@ -2,39 +2,43 @@ if(localStorage.getItem("likes") === null) {
   localStorage.setItem("likes", "[]");
 }
 
+function getLikesFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("likes")) || [];
+}
+
+export function applyLikesToPosts(posts) {
+  const likesDabase = getLikesFromLocalStorage();
+
+  likesDabase.forEach(({ index }) => {
+    const post = posts[index];
+    const icon = post.querySelector(".ph-heart");
+    icon.className = "ph-fill ph-heart";
+  })
+}
+
 export function addLike(index, posts) {
   const post = posts[index];
-  const like = post.querySelector(".ph-heart");
+  const icon = post.querySelector(".ph-heart");
   const title = post.querySelector("h2").innerText;
-  const likesDabase = JSON.parse(localStorage.getItem("likes"));
+  const likesDabase = getLikesFromLocalStorage();
   const data = {index, title};
   
   const includesLike = likesDabase.find(like => {
     return like.index === index;
   })
 
-  if(like.className === 'ph ph-heart') {
-    like.className = 'ph-fill ph-heart';
+  if(icon.className === 'ph ph-heart') {
+    icon.className = 'ph-fill ph-heart';
     
     if(!includesLike) {
       likesDabase.push(data);
       localStorage.setItem("likes", JSON.stringify(likesDabase));
     }
   } else {
-    like.className = 'ph ph-heart';
+    icon.className = 'ph ph-heart';
     const indexLocalStorage = likesDabase.findIndex(obj => obj.index === data.index);
     console.log(indexLocalStorage)
     likesDabase.splice(indexLocalStorage, 1);
     localStorage.setItem("likes", JSON.stringify(likesDabase));
   }
-}
-
-export function checkLike(posts) {
-  const likesDabase = JSON.parse(localStorage.getItem("likes"));
-
-  likesDabase.forEach(({ index }) => {
-    const post = posts[index];
-    const icone = post.querySelector(".ph-heart");
-    icone.className = "ph-fill ph-heart";
-  })
 }
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import filterPosts from "./filterPosts.js";
-import { addLike, checkLike } from "./likes.js";
+import { addLike, applyLikesToPosts } from "./likes.js";
 
 const input = document.querySelector(".search-field input");
 const posts = document.querySelectorAll("article");
@@ -10,4 +10,4 @@ posts.forEach((element, index) => {
   element.addEventListener('click', () => addLike(index, posts));
 })
 
-checkLike(posts);
+applyLikesToPosts(posts);
